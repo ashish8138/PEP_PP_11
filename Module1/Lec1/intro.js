28cdeff69f8dd39d9398bbcafc8fccdc6e7c7ded
ASHISHTHAKUR <ashishthakur8138@gmail.com>
2021-05-22 23:24:45 +0530

    objects in java script

---
+++ Module1/Lec1/intro.js
@@ -0,0 +1,22 @@
+let obj = {
+    name:"steve rogers",
+    place:"queens",
+    movies:["captain america", "winter soldier",{
+    bestie: "bucky",
+    nickname:"winter soldier",
+    partner: "falcon ",
+    weakness: ["brainwash"]  
+}]
+}
+console.log(obj.movies[2].weakness[0].substring(0,5));
+// console.log(obj.name);
+// console.log(obj.movies);
+ let key = "place";
+// console.log(obj["name"]);
+// // console.log(obj[key]);
+// // console.log(obj["place"]);
+ obj.skills = ["martial art" , "taekwondo"];
+obj.place = "new york";
+// // console.log(obj);
+
+
